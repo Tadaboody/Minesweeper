1c63e91043bdc9bfb85cfa75520f4b035e4e70ec
Tomer Keren <tomer.keren.dev@gmail.com>
2018-11-14 18:27:42 +0200

    Transferred to an external implementation of ZoomableView
    New features: This one works!

--- app/src/main/java/com/keren/tomer/minesweeper/GameActivity.kt
+++ app/src/main/java/com/keren/tomer/minesweeper/GameActivity.kt
@@ -2,6 +2,7 @@ package com.keren.tomer.minesweeper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-//        gameGrid.adapter = GameAdapter(this)
+        toasty_button.setOnClickListener { Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show() }
+        //        gameGrid.adapter = GameAdapter(this)
     }
 }
