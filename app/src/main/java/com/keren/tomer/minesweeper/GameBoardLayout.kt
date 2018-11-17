f8258aa7b01350ef6e040844b063512fae9bac16
Tomer Keren <tomer.keren.dev@gmail.com>
2018-11-17 14:17:08 +0200

    Update whole board after every move
    Also add an onHoldListener

--- app/src/main/java/com/keren/tomer/minesweeper/GameBoardLayout.kt
+++ app/src/main/java/com/keren/tomer/minesweeper/GameBoardLayout.kt
@@ -3,6 +3,7 @@ package com.keren.tomer.minesweeper
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewGroup
 import android.widget.GridLayout
 import android.widget.GridLayout.spec
 import android.widget.ImageView
@@ -21,17 +22,31 @@ fun GridLayout.addGame(game: Game) {
 fun GridLayout.createView(game: Game, item: DoublyIndexedItem<Tile>): View {
     return ImageView(context).apply {
         setImageResource(R.drawable.hidden_tile)
+        setOnLongClickListener {
+            game.holdTile(item.i, item.j)
+            updateBoard(game)
+            true
+        }
         setOnClickListener {
             game.clickTile(item.i, item.j)
-            val image = when (item.value.state) {
-                    Tile.State.HIDDEN -> R.drawable.hidden_tile
-                    Tile.State.FLAGGED -> R.drawable.flag_tile
-                    Tile.State.MINE -> R.drawable.mine_tile
-                    Tile.State.NUMBERED -> R.mipmap.ic_launcher
-                }
-            setImageResource(image)
+            updateBoard(game)
             Toast.makeText(context, item.value.toString(), Toast.LENGTH_SHORT).show()
         }
+    }
+}
+
+fun ViewGroup.children(): List<View> = (0 until childCount).map { getChildAt(it) }
+
+fun GridLayout.updateBoard(game: Game) {
+    game.board.flatten().zip(children()).forEach { (item, view) ->
+        val image = when (item.value.state) {
+            Tile.State.HIDDEN -> R.drawable.hidden_tile
+            Tile.State.FLAGGED -> R.drawable.flag_tile
+            Tile.State.MINE -> R.drawable.mine_tile
+            Tile.State.NUMBERED -> R.mipmap.ic_launcher
+        }
+        view as ImageView
+        view.setImageResource(image)
     }
 }
 
